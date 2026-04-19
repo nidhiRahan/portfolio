66bdcd134552b56b848af882dc547039c02c5377
Nidhi <rahangdalenidhi06@gmail.com>
2026-04-19 17:05:12 +0530

    remove tritther and home page update

--- src/Data/SocialData.js
+++ src/Data/SocialData.js
@@ -1,14 +1,13 @@
 const githubLink = "https://github.com/nidhirahan";
 const instaLink = "https://www.instagram.com/rahangdale_nidhi__06?igsh=ZXBlM3RpMjYyc211";
 const linkedInLink = "https://www.linkedin.com/in/nrrn/";
-const twitterLink = "https://twitter.com/nidhirahan";
+//const twitterLink = "https://twitter.com/nidhirahan";
 const emailLink = "mailto:rahangdalenidhio8@gmail.com";
 
 const profileLinks = {
     githubLink,
     instaLink,
     linkedInLink,
-    twitterLink,
     emailLink
 }
 const SocialData = profileLinks;
--- src/components/SocialLinks/SocialLinks.js
+++ src/components/SocialLinks/SocialLinks.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import SocialData from "../../Data/SocialData";
 import GitHubIcon from '@mui/icons-material/GitHub';
-import TwitterIcon from '@mui/icons-material/Twitter';
+//import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 // import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -17,7 +17,6 @@ const SocialLinks = (props) => {
         <div className={`${classes.socialLinks} ${props.className}`} style={{color:nonThemeColor}}>
             <a href={SocialData.githubLink} target="_blank" rel="noreferrer" ><GitHubIcon fontSize="large" /></a>
             <a href={SocialData.linkedInLink} target="_blank" rel="noreferrer"><LinkedInIcon fontSize="large" /></a>
-            <a href={SocialData.twitterLink} target="_blank" rel="noreferrer"><TwitterIcon fontSize="large" /></a>
             <a href={SocialData.instaLink} target="_blank" rel="noreferrer"><InstagramIcon fontSize="large" /></a>
             <a href={SocialData.emailLink} target="_blank" rel="noreferrer"><EmailIcon fontSize="large" /></a>
         </div>
